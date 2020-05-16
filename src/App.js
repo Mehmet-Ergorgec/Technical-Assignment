1758b8690c3bd1da427b9c9ef4e857c62ba275ff
20twa07 <48910851+20twa07@users.noreply.github.com>
2020-05-16 15:29:36 +0200

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, createRef, useReducer } from 'react';
+import SearchBar from './components/Searchbar/SearchBar';
+import Users from './components/Users/Users';
+import { data } from './data/users';
+import { SearchContext } from './shared/context/search-context';
+import inputReducer from './shared/Reducers/inputReducer';
+import { CLICK, SEARCHBAR } from './shared/Actions/types';
+
 import './App.css';
 
 function App() {
+  const onClickWindow = createRef();
+
+  const [state, dispatch] = useReducer(inputReducer, {
+    showDropdown: false,
+    searchText: '',
+    label: 'Contact',
+  });
+
+  const users = data.filter((user) => {
+    if (user.name === null) {
+      return 'No user found';
+    }
+    return user.name.toLowerCase().includes(state.searchText.toLowerCase());
+  });
+
+  const onClickOutsideHandler = () => {
+    if (state.showDropdown && onClickWindow.current) {
+      dispatch({ type: CLICK, label: 'Contact' });
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('click', onClickOutsideHandler);
+
+    return () => {
+      window.removeEventListener('click', onClickOutsideHandler);
+    };
+  });
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    const { value } = event.target;
+    dispatch({ type: SEARCHBAR, value: value });
+  };
+
+  const handleInputClick = () => {
+    dispatch({ type: CLICK, label: 'Contact' });
+  };
+
+  const handleUserClick = (username) => {
+    dispatch({ type: CLICK, label: '' });
+    dispatch({ type: SEARCHBAR, value: username });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SearchContext.Provider
+      value={{
+        handleChange: handleChange,
+        handleInputClick: handleInputClick,
+        handleUserClick: handleUserClick,
+      }}
+    >
+      <div ref={onClickWindow} className='search-bar-wrapper'>
+        <SearchBar
+          name='search'
+          value={state.searchText}
+          label={state.label}
+          placeholder='Type or search...'
+        />
+        {state.showDropdown && <Users data={users} />}
+      </div>
+    </SearchContext.Provider>
   );
 }
 
